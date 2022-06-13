01269326f1c176fca857a09c9118c8b3821a073e
Jacp <jacopoabate123@gmail.com>
2022-06-13 07:48:02 +0000

    creazione modelli

--- Progetto/polls/admin.py
+++ Progetto/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import  Coffee, Merchant, User
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Merchant)
+admin.site.register(Coffee)
--- Progetto/polls/models.py
+++ Progetto/polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.forms import CharField, DateField
 
 # Create your models here.
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True),
+    full_name = CharField(max_length=200),
+    created_at = DateField(),
+    country_code = models.IntegerField()
+
+class Merchant(models.Model):
+    id = models.BigAutoField(primary_key=True),
+    country_code = models.IntegerField(),
+    merchant_name = CharField(max_length=None),
+    admin_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Coffee(models.Model):
+    id = models.BigAutoField(primary_key=True),
+    name = models.CharField(max_length=200),
+    producer = models.CharField(max_length=200),
+    producer_id = models.ForeignKey(Merchant, on_delete=models.CASCADE),
+    origin = models.CharField(max_length=200),
+    is_it_speciality = models.BooleanField(),
+    toast_level = models.IntegerField(),
+    is_it_available = models.BooleanField()
