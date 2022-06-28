7b41e7afb99ab40e462a13d6fd5ee131d73ccc43
Jacp <jacopoabate123@gmail.com>
2022-06-28 10:23:56 +0000

    urls and views

---
+++ Progetto/polls/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('datetime', views.current_datetime, name='current_datetime'),
+    path('', views.index, name='index'),
+]
