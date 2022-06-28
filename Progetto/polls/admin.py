e944f97d909a3922353629d1c9002d02fa59efa8
Jacp <jacopoabate123@gmail.com>
2022-06-28 10:23:42 +0000

    all changes

--- Progetto/polls/admin.py
+++ Progetto/polls/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import  Coffee, Merchant, User
+from .models import  Coffee, Merchant, User, CoffeePlantType, Process
 
 # Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'mail',)
+ 
+class MerchantAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at')
+    list_display = ('country_code', 'merchant_name')
+
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'seller', 'producer', 'varieties', 'origin_country', 'process', 'toast_level', 
+                        'tasting_notes', 'is_it_speciality')
+    fieldsets = (
+        (None, {
+            'fields' : ('name','seller','seller_id','origin_country','process','is_it_speciality','is_it_available',
+                        'url_to_product',)
+        }),
+        ('More informations', {
+            'classes': ('collapse',),
+            'fields' : ('producer', 'tasting_notes', 'varieties', 'toast_level', 'how_many_are_available', 'origin_country_2', 'process_2',
+                        'origin_area', 'origin_area_2', 'price', 'producer_2')
+        }),
+    )
+    list_filter = ('seller', 'origin_country', 'process')
+    
+                
+admin.site.register(User, UserAdmin)
 admin.site.register(Merchant)
-admin.site.register(Coffee)
+admin.site.register(Coffee, CoffeeAdmin)
+admin.site.register(CoffeePlantType)
+admin.site.register(Process)
--- Progetto/polls/models.py
+++ Progetto/polls/models.py
@@ -1,25 +1,61 @@
 from django.db import models
-from django.forms import CharField, DateField
 
 # Create your models here.
 class User(models.Model):
-    id = models.BigAutoField(primary_key=True),
-    full_name = CharField(max_length=200),
-    created_at = DateField(),
-    country_code = models.IntegerField()
+    full_name = models.CharField(max_length=200, null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
+    country_code = models.IntegerField(null=True)
+    mail = models.EmailField(max_length=254, null=True)
+
+    def __str__(self):
+        return self.full_name
 
 class Merchant(models.Model):
-    id = models.BigAutoField(primary_key=True),
-    country_code = models.IntegerField(),
-    merchant_name = CharField(max_length=None),
-    admin_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    country_code = models.IntegerField(null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
+    merchant_name = models.CharField(max_length=200, null=True)
+    admin_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.merchant_name
+
+class CoffeePlantType(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    origin = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Process(models.Model):
+    name = name = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Processes"
 
 class Coffee(models.Model):
-    id = models.BigAutoField(primary_key=True),
-    name = models.CharField(max_length=200),
-    producer = models.CharField(max_length=200),
-    producer_id = models.ForeignKey(Merchant, on_delete=models.CASCADE),
-    origin = models.CharField(max_length=200),
-    is_it_speciality = models.BooleanField(),
-    toast_level = models.IntegerField(),
-    is_it_available = models.BooleanField()
+    name = models.CharField(max_length=200, null=True)
+    producer = models.CharField(max_length=200, null=True, blank = True)
+    producer_2 = models.CharField(max_length=200, null=True)
+    seller = models.CharField(max_length=200, null=True)
+    seller_id = models.ForeignKey(Merchant, on_delete=models.CASCADE, null=True)
+    origin_country = models.CharField(max_length=200, null=True)
+    origin_country_2 = models.CharField(max_length=200, null=True)
+    origin_area = models.CharField(max_length=200, null=True)
+    origin_area_2 = models.CharField(max_length=200, null=True)
+    process = models.ForeignKey(Process, on_delete=models.CASCADE, null=True, related_name= 'first_process')
+    process_2 = models.ForeignKey(Process, on_delete=models.CASCADE, null=True)
+    tasting_notes = models.CharField(max_length=200, null=True, blank = True)
+    varieties = models.ForeignKey(CoffeePlantType, on_delete=models.CASCADE, null=True, blank = True)
+    is_it_speciality = models.BooleanField(null=True)
+    toast_level = models.IntegerField(null=True, blank = True)
+    is_it_available = models.BooleanField(null=True)
+    how_many_are_available = models.IntegerField(null=True, blank = True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank = True)
+    url_to_product = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
